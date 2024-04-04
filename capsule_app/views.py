2dbd485e47a8627905adc4a14e195eddb0f3fb92
NathanLindeman93 <Nate123561@msn.com>
2024-04-03 21:17:14 -0600

    Template/index commit

--- capsule_app/views.py
+++ capsule_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from django.views import generic
+from .forms import *
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'console_app/index.html')
